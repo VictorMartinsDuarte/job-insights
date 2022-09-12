f81efe55ed69e21fa62883cbe60b76a9f1bb8e3e
Victor Martins <victor.martins.duarte@gmail.com>
2022-09-12 18:56:09 -0300

    Feat: Req-02 Adds logic to function get_unique_job_types

--- src/insights.py
+++ src/insights.py
@@ -1,19 +1,13 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+    jobs_data = read(path)
+    jobs_types_unique = []
+    for obj in jobs_data:
+        if obj['job_type'] not in jobs_types_unique:
+            jobs_types_unique.append(obj['job_type'])
+    return jobs_types_unique
 
 
 def filter_by_job_type(jobs, job_type):
--- src/jobs.py
+++ src/jobs.py
@@ -5,9 +5,10 @@ import csv
 @lru_cache
 def read(path):
     with open(path, mode='r') as csv_data:
-        dictData = csv.DictReader(csv_data, delimiter=',')
-        listData = list(dictData)
-        return listData
+        dict_data = csv.DictReader(csv_data, delimiter=',')
+        list_data = list(dict_data)
+        return list_data
+    # Ref: https://linuxhint.com/import-csv-to-list-python/
     # """Reads a file from a given path and returns its contents
 
     # Parameters
